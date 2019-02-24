9f7378606e3820a8177b661c469bb0150aa9a6dc
Zuber00124 <zuber00124@gmail.com>
2019-02-24 11:54:02 +0530

    3 Navbar Styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const AboutPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Home Page</h1>
+    <h1>About us.</h1>
   </Layout>
 )
 
-export default IndexPage
+export default AboutPage
